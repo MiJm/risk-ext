aa987306d8857b3f65e28c897a1504fba1e1c4ba
jijijianfeng <jijijianfeng@jijijianfengdeMacBook-Air.local>
2018-03-30 15:57:56 +0800

    违章调整

--- models/Peccancy.go
+++ models/Peccancy.go
@@ -1,15 +1,21 @@
 package models
 
 import (
+	"crypto/tls"
+
+	"github.com/astaxie/beego/httplib"
 	"gopkg.in/mgo.v2/bson"
 )
 
 type Peccancies struct {
-	PeccancyId  bson.ObjectId `bson:"_id,omitempty" json:"peccancy_id"`                   //id
-	TotalFine   float32       `bson:"peccancy_total_fine" json:"peccancy_total_fine"`     //未处理违章总罚款
-	TotalPoints uint8         `bson:"peccancy_total_points" json:"peccancy_total_points"` //未处理违章总扣分
-	Untreated   uint8         `bson:"peccancy_untreated" json:"peccancy_untreated"`       //未处理违章条数
-1
+	Model          `bson:"-" json:"-"` //model基类
+	PeccancyId     bson.ObjectId       `bson:"_id,omitempty" json:"peccancy_id"`                   //id
+	TotalFine      float32             `bson:"peccancy_total_fine" json:"peccancy_total_fine"`     //未处理违章总罚款
+	TotalPoints    uint8               `bson:"peccancy_total_points" json:"peccancy_total_points"` //未处理违章总扣分
+	Untreated      uint8               `bson:"peccancy_untreated" json:"peccancy_untreated"`       //未处理违章条数
+	PeccanciesInfo []Info              `bson:"peccancy_info" json:"peccancy_info"`                 //违章时间
+	QueryDate      uint32              `bson:"query_date" json:"query_date"`                       //查询日期
+	CarPlate       string              `bson:"car_plate" json:"car_plate"`                         //车牌号
 }
 
 type Info struct {
@@ -23,4 +29,37 @@ type Info struct {
 	PeccancyCity          string  `bson:"peccancy_city" json:"peccancy_city"`                     //城市
 	PeccancyViolationNum  string  `bson:"peccancy_violation_num" json:"peccancy_time"`            //违章官方条码
 	PeccancyStatus        int8    `bson:"peccancy_status" json:"peccancy_status"`                 //违章缴费状态
+}
+
+//阿里云获取违章支持的城市接口
+func GetPeccancyCity() (rs string) {
+	appcode := "5ba0770c939048b98fed07083106988a"
+	url := "http://ddycapi.market.alicloudapi.com/violation/condition"
+	req := httplib.Get(url)
+	req.Header("Authorization", "APPCODE "+appcode)
+	rs, err := req.String()
+	if err != nil {
+		rs = ""
+	}
+	return
+}
+
+//阿里云违章查询的接口
+func GetPeccancy(carNum, vin, engineNo, city, carType string) string {
+	appcode := "5ba0770c939048b98fed07083106988a"
+	url := "http://ddycapi.market.alicloudapi.com/violation/query"
+	req := httplib.Post(url)
+	req.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
+	req.Header("Content-Type", "application/json;charset=UTF-8")
+	req.Header("Authorization", "APPCODE "+appcode)
+	req.Param("plateNumber", carNum)
+	req.Param("vin", vin)
+	req.Param("engineNo", engineNo)
+	req.Param("carType", carType)
+	req.Param("city", city)
+	rs, err := req.String()
+	if err != nil {
+		return ""
+	}
+	return rs
 }
