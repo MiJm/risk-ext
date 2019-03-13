4b280575183781550d0eabaf99044bc4765ab351
jijijianfeng <jijijianfeng@jijijianfengdeMacBook-Air.local>
2019-03-13 16:48:02 +0800

    预警新增类型

--- models/Alarms.go
+++ models/Alarms.go
@@ -25,7 +25,7 @@ type Alarms struct {
 	Alarm_pen_id       string              `json:"alarm_pen_id"`                       //报警围栏ID
 	Alarm_latlng       Latlng              `json:"alarm_latlng"`                       //报警坐标
 	Alarm_address      string              `json:"alarm_address"`                      //报警地址
-	Alarm_type         uint8               `json:"alarm_type"`                         //警报类型 0 断电报警（有线） 1见光报警（无线）  2围栏（出围） 3围栏（入围）  4关机报警 5开机报警 6低电报警 7通电报警 8见光恢复 9低电恢复 10解除出围 11解除入围 12风险点预警 13离线预警
+	Alarm_type         uint8               `json:"alarm_type"`                         //报警类型 0 断电报警（有线） 1见光报警（无线）  2围栏（出围） 3围栏（入围）  4关机报警 5开机报警 6低电报警 7通电报警 8见光恢复 9低电恢复 10解除出围 11解除入围,12风险点预警,13离线报警，14异动报警(离线后上线位置与上次上报差距大)，15震动报警，16拆卸报警，17acc关报警，18acc开报警 19停车超时
 	Alarm_loctype      uint8               `json:"alarm_loctype"`                      //报警定位类型 0gps 1基站 2WiFi
 	Alarm_read         uint8               `json:"alarm_read"`                         //0未读；1已读
 	AlarmUserId        string              `bson:"alarm_user_id" json:"alarm_user_id"` //报警用户ID
@@ -34,13 +34,14 @@ type Alarms struct {
 	Alarm_desc         string              `json:"alarm_desc"`                         //报警描述()
 }
 
-var Alarm_type_string = []string{"断电/关机预警", "通电/开机预警", "光感预警", "出围预警", "入围预警", "低电量预警", "风险点预警", "离线预警", "异动报警"}
-var Alarm_type_color = []string{"#F0644E", "#3ED080", "#F8AD2E", "#886EFF", "#886EFF", "#F78C75", " #F66A00", "#F53E35", "#3A9CFF"}
+var Alarm_type_string = []string{"断电/关机预警", "通电/开机预警", "光感预警", "出围预警", "入围预警", "低电量预警", "风险点预警", "离线预警", "异动报警", "停车超时"}
+var Alarm_type_color = []string{"#F0644E", "#3ED080", "#F8AD2E", "#886EFF", "#886EFF", "#F78C75", " #F66A00", "#F53E35", "#3A9CFF", "#32C5FF"}
 
-var Alarm_descrip = []bson.M{{"type": "断电", "color": "#F0644E", "discription": "电源被切断", "args": 0}, {"type": "见光", "color": "#F8AD2E", "discription": "检测到光感报警", "args": 0}, {"type": "出围", "color": "#886EFF", "discription": "驶出围栏[%s]", "args": 1}, {"type": "入围", "color": "#886EFF", "discription": "驶入围栏[%s]", "args": 1},
-	{"type": "关机", "color": "#F0644E", "discription": "已关机", "args": 0}, {"type": "开机", "color": "#3ED080", "discription": "关机报警解除，已重新开机", "args": 0}, {"type": "低电量", "color": "#F78C75", "discription": "电量少于20%", "args": 0}, {"type": "通电", "color": "#3ED080", "discription": "断电报警解除，已恢复通电", "args": 0},
-	{"type": "见光恢复", "color": "#F8AD2E", "discription": "光感报警解除，已复位", "args": 0}, {"type": "低电恢复", "color": "#F78C75", "discription": "低电报警解除，已复位", "args": 0}, {"type": "出围解除", "color": "#886EFF", "discription": "围栏报警解除", "args": 0}, {"type": "入围解除", "color": "#886EFF", "discription": "围栏报警解除", "args": 0},
-	{"type": "风险点", "color": "#F66A00", "discription": "进入风险点[%s]", "args": -1}, {"type": "离线", "color": "#F53E35", "discription": "离线报警", "args": 0}, {"type": "异动", "color": "#3A9CFF", "discription": "异动报警", "args": 0}}
+var Alarm_descrip = []bson.M{{"type": "断电", "color": "#F0644E", "discription": "电源被切断", "args": 0, "isShow": true}, {"type": "见光", "color": "#F8AD2E", "discription": "检测到光感报警", "args": 0, "isShow": true}, {"type": "出围", "color": "#886EFF", "discription": "驶出围栏[%s]", "args": 1, "isShow": true}, {"type": "入围", "color": "#886EFF", "discription": "驶入围栏[%s]", "args": 1, "isShow": true},
+	{"type": "关机", "color": "#F0644E", "discription": "已关机", "args": 0, "isShow": true}, {"type": "开机", "color": "#3ED080", "discription": "关机报警解除，已重新开机", "args": 0, "isShow": true}, {"type": "低电量", "color": "#F78C75", "discription": "电量少于20%", "args": 0, "isShow": true}, {"type": "通电", "color": "#3ED080", "discription": "断电报警解除，已恢复通电", "args": 0, "isShow": true},
+	{"type": "见光恢复", "color": "#F8AD2E", "discription": "光感报警解除，已复位", "args": 0, "isShow": true}, {"type": "低电恢复", "color": "#F78C75", "discription": "低电报警解除，已复位", "args": 0, "isShow": true}, {"type": "出围解除", "color": "#886EFF", "discription": "围栏报警解除", "args": 0, "isShow": true}, {"type": "入围解除", "color": "#886EFF", "discription": "围栏报警解除", "args": 0, "isShow": true},
+	{"type": "风险点", "color": "#F66A00", "discription": "进入风险点[%s]", "args": -1, "isShow": true}, {"type": "离线", "color": "#F53E35", "discription": "离线报警", "args": 0, "isShow": true}, {"type": "异动", "color": "#3A9CFF", "discription": "异动报警", "args": 0, "isShow": true}, {"type": "震动", "color": "#3A9CFF", "discription": "震动报警", "args": 0, "isShow": false}, {"type": "拆卸", "color": "#3A9CFF", "discription": "拆卸报警", "args": 0, "isShow": false},
+	{"type": "ACC关", "color": "#3A9CFF", "discription": "ACC关报警", "args": 0, "isShow": false}, {"type": "ACC开", "color": "#3A9CFF", "discription": "ACC开报警", "args": 0, "isShow": false}, {"type": "停车超时", "color": "#32C5FF", "discription": "车辆停车已超过24小时", "args": 0, "isShow": true}}
 
 func (this *Alarms) GetAlarmListByUserId(userId, deviceId string, page, pageSize int) (rs []Alarms, count int, err error) {
 	if page < 1 {
